2397498e9be8a8e519ec13bff5f8bb0b5638da1b
Ralph Schaer <ralphschaer@gmail.com>
2015-03-04 17:34:31 +0100

    Upgrade to spring boot 1.2.2 and remove custom compression code. This is now built in spring boot

--- src/main/java/ch/rasc/dataformat/Application.java
+++ src/main/java/ch/rasc/dataformat/Application.java
@@ -8,14 +8,10 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.msgpack.MessagePack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -25,7 +21,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SpringBootApplication
 public class Application extends WebMvcConfigurerAdapter {
 
-	// -Dspring.profiles.active=compression
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -71,25 +66,6 @@ public class Application extends WebMvcConfigurerAdapter {
 				.mediaType("cbor", MediaType.valueOf("application/cbor"))
 				.mediaType("msgpack", MediaType.valueOf("application/x-msgpack"))
 				.mediaType("csv", MediaType.valueOf("text/csv"));
-	}
-
-	@Bean
-	@Profile("compression")
-	public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
-		return servletContainer -> ((TomcatEmbeddedServletContainerFactory) servletContainer)
-				.addConnectorCustomizers(connector -> {
-					AbstractHttp11Protocol<?> httpProtocol = (AbstractHttp11Protocol<?>) connector
-							.getProtocolHandler();
-					httpProtocol.setCompression("on");
-					httpProtocol.setCompressionMinSize(512);
-					String mimeTypes = httpProtocol.getCompressableMimeTypes();
-					String additionalMimeTypes = mimeTypes + ","
-							+ MediaType.APPLICATION_JSON_VALUE + ","
-							+ MediaType.APPLICATION_XML_VALUE + ","
-							+ "application/cbor,application/x-msgpack,text/csv";
-
-					httpProtocol.setCompressableMimeTypes(additionalMimeTypes);
-				});
 	}
 
 }
